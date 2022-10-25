28c5df98ba7eb2049ec80cbc3ef1de4f6c2b76d5
AlcatrazEscapee <alex@molleroneill.com>
2022-10-25 14:14:41 -0400

    Refactor into LogLine and LogException classes

--- src/engine.ts
+++ src/engine.ts
@@ -1,3 +1,4 @@
+import { LogException, LogLine } from "./logs";
 import { IOption, IOptions, Options } from "./options";
 import { getElementById, requireType } from "./utils";
 
@@ -26,295 +27,154 @@ export class Engine {
 
     public load(text: string) {
 
+        // Parse log lines
         let lines: string[] = text.split('\n');
-
         for (let i = 0; i < lines.length; i++) {
             let line: string = lines[i] as string;
-
-            let time: string | null = null;
-            let source: string | null = null;
-            let level: LogLevel | null = null;
-
-            let message: string = line;
-
-            const timeMatch = /^\[([0-9]+:[0-9]+:[0-9]+)\] /.exec(message);
-            if (timeMatch !== null) {
-                time = timeMatch[1] as string;
-                message = message.substring((timeMatch[0] as string).length);
-            }
-
-            const sourceLevelMatch = /^\[([^\/]+)\/(INFO|DEBUG|WARN|ERROR|FATAL)\]: /.exec(message);
-            if (sourceLevelMatch !== null) {
-                source = sourceLevelMatch[1] as string;
-                level = sourceLevelMatch[2] as LogLevel;
-                message = message.substring((sourceLevelMatch[0] as string).length);
-            }
-
-            const pTag = document.createElement('p');
-            
-            applyLevelToTag(pTag, level);
+            let {time, source, level, message}: PrefixParseResult = parsePrefixLine(line);
 
             // Inspect if this is the first line of a stack trace
             const exceptionMatch = /^([A-Za-z0-9\.]+): /.exec(line);
             if (exceptionMatch !== null) {
-                let prevLogLine = this.logLines[this.logLines.length - 1] as LogLine;
 
-                let exceptionType = exceptionMatch[1] as string;
-                let exceptionMessage = message.substring((exceptionMatch[0] as string).length);
+                let prev: LogLine | null = this.peek();
+                if (prev === null) {
+                    continue;
+                }
 
-                let exceptionTag = document.createElement('p');
+                const exceptionType: string = exceptionMatch[1] as string;
+                const exceptionMessage: string = message.substring((exceptionMatch[0] as string).length);
 
-                applyLevelToTag(exceptionTag, prevLogLine.level);
+                const exception: LogException = prev.pushException(exceptionType);
+                let latestException: LogException = exception;
 
-                let exception: ExceptionInfo = {
-                    type: exceptionType,
-                    message: exceptionMessage,
-
-                    stacktrace: [],
-                    cause: null,
-
-                    tag: exceptionTag
-                };
-                let latestException: ExceptionInfo = exception;
+                exception.push(exceptionMessage)
 
                 // Capture stack trace(s)
                 for (i++; i < lines.length; i++)
                 {
                     const stackTraceLine = lines[i] as string;
-                    const stackTraceMatch = /^[ \t]+at /.exec(stackTraceLine);
-                    if (stackTraceMatch !== null) {
-
-                        const stackTraceTag = document.createElement('p');
-                        
-                        applyLevelToTag(stackTraceTag, prevLogLine.level);
-
-                        const stackTraceInfo: LineInfo = {
-                            line: stackTraceLine,
-                            tag: stackTraceTag
-                        }
-
-                        latestException.stacktrace.push(stackTraceInfo);
-                        continue;
-                    }
-
-                    const longStackTraceMatch = /^[ \t]+\.\.\. [0-9]+ more/.exec(stackTraceLine);
-                    if (longStackTraceMatch !== null) {
-                        const stackTraceTag = document.createElement('p');
-                        
-                        applyLevelToTag(stackTraceTag, prevLogLine.level);
-
-                        const stackTraceInfo: LineInfo = {
-                            line: stackTraceLine,
-                            tag: stackTraceTag
-                        }
-
-                        latestException.stacktrace.push(stackTraceInfo);
+                    if (/^[ \t]+at /.test(stackTraceLine) || /^[ \t]+\.\.\. [0-9]+ more/.test(stackTraceLine)) {
+                        latestException.pushTrace(stackTraceLine);
                         continue;
                     }
 
                     const causeMatch = /^Caused by: ([A-Za-z0-9\.]+): /.exec(stackTraceLine);
                     if (causeMatch !== null) {
-
                         const causeType: string = causeMatch[1] as string;
                         const causeMessage: string = stackTraceLine.substring((causeMatch[0] as string).length);
 
-                        const causeTag = document.createElement('p');
-                        
-                        applyLevelToTag(causeTag, prevLogLine.level);
-
-                        latestException.cause = {
-                            type: causeType,
-                            message: causeMessage,
-
-                            stacktrace: [],
-                            cause: null,
-
-                            tag: causeTag
-                        };
-                        latestException = latestException.cause;
+                        latestException = latestException.pushCause(causeType);
+                        latestException.push(causeMessage);
                         continue;
                     }
 
-                    i--;
+                    let futurePrefix: PrefixParseResult = parsePrefixLine(stackTraceLine);
+
+                    if (futurePrefix.time === null && futurePrefix.level === null && futurePrefix.source === null && latestException.trace.length == 0) {
+                        // If we hit a recognized line, then we abort parsing.
+                        // Otherwise, we accumulate lines as part of the exception message *if* it's not already part of the stack trace.
+                        latestException.push(stackTraceLine);
+                        continue;
+                    }
+
+                    // Did not recognize this line; abort and continue;
                     break;
                 }
-
-                prevLogLine.exception.push(exception);
+                i--;
                 continue;
             }
 
-            if (level === 'FATAL') {
-                level = 'ERROR';
-            }
-
-            if (time === null && source === null && level === null) {
-                // Append to previous line
-
-                let prevLogLine = this.logLines[this.logLines.length - 1] as LogLine;
-
-                prevLogLine.message.push({
-                    line: message,
-                    tag: pTag
-                })
+            if (time === null || source === null || level === null) {
+                this.peek()?.push(message);
                 continue;
             }
 
-            this.logLines.push({
-                message: [{
-                    line: message,
-                    tag: pTag
-                }],
-                time,
-                source,
-                level,
-                tag: pTag,
-                exception: []
-            });
+            const logLine = LogLine.create(time, source, level);
+            logLine.push(message);
+
+            this.logLines.push(logLine);
         }
 
-        for (let line of this.logLines) {
-            update(line, this.options);
-            this.logTag.appendChild(line.tag);
-            for (let e of line.exception) {
-                addExceptionInfo(e, this.logTag);
-            }
-        }
+        // Build the HTML view
+        this.build();
+
+        // Update based on the default UI options
+        this.update();
 
         // Update stack trace display
         let count = 0;
         for (let line of this.logLines) {
-            if (line.exception.length > 0) count++;
+            count += line.exceptions.length;
         }
         this.stackTraceCountTag.innerText = `(${count})`;
     }
 
     public set(key: IOption, value: boolean): void {
-
         this.options[key] = value;
+        this.update();
+    }
 
+    private build(): void {
         for (let line of this.logLines) {
-            update(line, this.options);
-        }
-    }
-}
-
-function addExceptionInfo(e: ExceptionInfo | null, tag: HTMLDivElement) {
-    if (e !== null) {
-        tag.appendChild(e.tag);
-        for (let st of e.stacktrace) {
-            tag.appendChild(st.tag);
-        }
-        addExceptionInfo(e.cause, tag);
-    }
-}
-
-function applyLevelToTag(tag: HTMLParagraphElement, level: LogLevel | null) {
-    if (level === 'DEBUG') {
-        tag.classList.add('log-debug');
-    } else if (level === 'INFO') {
-        tag.classList.add('log-info');
-    } else if (level === 'WARN') {
-        tag.classList.add('log-warn');
-    } else if (level === 'ERROR' || level === 'FATAL') {
-        tag.classList.add('log-error');
-    }
-}
-
-function update(line: LogLine, options: IOptions) {
-
-    const hideLevel: boolean = !options.levelFor(line.level, true);
-
-    line.tag.hidden = hideLevel;
-
-    let message: string = (line.message[0] as LineInfo).line;
-
-    if (options.source || options.level) {
-        if (!options.source) {
-            // Only level
-            if (line.level !== null) {
-                message = `[${line.level}] ${message}`;
-            }
-        } else if (!options.level) {
-            // Only source
-            if (line.source !== null) {
-                message = `[${line.source}] ${message}`;
-            }
-        } else {
-            // Both
-            if (line.level !== null && line.source !== null) {
-                message = `[${line.source} / ${line.level}] ${message}`;
-            } else if (line.level !== null) {
-                message = `[${line.level}] ${message}`;
-            } else if (line.source !== null) {
-                message = `[${line.source}] ${message}`;
-            }
+            line.build(this.logTag);
         }
     }
 
-    if (options.timestamps && line.time !== null) {
-        message = `[${line.time}] ${message}`;
-    }
-
-    message = encodeHtml(message);
-    
-    (line.message[0] as LineInfo).tag.innerHTML = message;
-
-    for (let i = 1; i < line.message.length; i++) {
-        let messageLine: LineInfo = line.message[i] as LineInfo;
-
-        messageLine.tag.hidden = hideLevel;
-        messageLine.tag.innerHTML = encodeHtml(messageLine.line);
-    }
-
-    for (let e of line.exception) {
-        e.tag.hidden = !options.stacktrace || hideLevel;
-
-        e.tag.innerHTML = encodeHtml(`${e.type}: ${e.message}`);
-
-        let e0: ExceptionInfo | null = e;
-        while (e0 !== null) {
-            for (let st of e0.stacktrace) {
-                st.tag.hidden = !options.stacktrace || hideLevel;
-                st.tag.innerHTML = encodeHtml(st.line);
-            }
-
-            e0 = e0.cause;
-            if (e0 !== null) {
-                e0.tag.hidden = hideLevel;
-                e0.tag.innerHTML = encodeHtml(`Caused by: ${e0.type}: ${e0.message}`);
-            }
+    private update(): void {
+        for (let line of this.logLines) {
+            line.update(this.options);
         }
     }
+
+    private peek(): LogLine | null { 
+        const line: LogLine | undefined = this.logLines[this.logLines.length - 1];
+        return line === undefined ? null : line;
+    }
 }
 
-function encodeHtml(text: string): string {
-    return text.replaceAll('\t', '    ').replaceAll(' ', '&nbsp;');
-}
-
-type LogLine = {
-    message: LineInfo[],
+type PrefixParseResult = {
     time: string | null,
     source: string | null,
     level: LevelInfo | null,
+    message: string
+};
 
-    exception: ExceptionInfo[]
+function parsePrefixLine(line: string): PrefixParseResult {
+    let time: string | null = null;
+    let source: string | null = null;
+    let level: LevelInfo | null = null;
 
-    tag: HTMLParagraphElement
+    let message: string = line;
+
+    const timeMatch = /^\[([0-9]+:[0-9]+:[0-9]+)\] /.exec(message);
+    if (timeMatch !== null) {
+        time = timeMatch[1] as string;
+        message = message.substring((timeMatch[0] as string).length);
+    }
+
+    const sourceLevelMatch = /^\[([^\/]+)\/(INFO|DEBUG|WARN|ERROR|FATAL)\]: /.exec(message);
+    if (sourceLevelMatch !== null) {
+        source = sourceLevelMatch[1] as string;
+        level = asLevelInfo(sourceLevelMatch[2]);
+        message = message.substring((sourceLevelMatch[0] as string).length);
+    }
+
+    return {
+        time,
+        source,
+        level,
+        message
+    };
 }
 
-type ExceptionInfo = {
-    type: string,
-    message: string,
 
-    stacktrace: LineInfo[]
-    cause: ExceptionInfo | null,
-
-    tag: HTMLParagraphElement,
+function asLevelInfo(text: any): LevelInfo | null {
+    if (text === 'DEBUG' || text === 'INFO' || text === 'WARN' || text === 'ERROR') {
+        return text;
+    } else if (text === 'FATAL') {
+        return 'ERROR';
+    }
+    console.log(`Not a LevelInfo: '${text}'`);
+    return null;
 }
-
-type LineInfo = {
-    line: string,
-    tag: HTMLParagraphElement
-}
-
-type LogLevel = 'DEBUG' | 'INFO' | 'WARN' | 'ERROR' | 'FATAL';
---
+++ src/logs.ts
@@ -0,0 +1,215 @@
+import { IOptions } from "./options";
+
+
+export interface ILine {
+    readonly text: string;
+    readonly tag: HTMLParagraphElement;
+}
+
+export interface IException {
+    readonly type: string;
+    readonly message: ILine[];
+
+    readonly trace: ILine[];
+
+    cause: IException | null;
+}
+
+export class LogException {
+
+    public static create(type: string, level: LevelInfo): LogException {
+        return new LogException(type, level);
+    }
+
+    public readonly type: string;
+    private readonly level: LevelInfo;
+    
+    public readonly message: ILine[];
+    public readonly trace: ILine[];
+
+    public cause: LogException | null;
+
+    private constructor(type: string, level: LevelInfo) {
+        this.type = type;
+        this.level = level;
+        
+        this.message = [];
+        this.trace = [];
+
+        this.cause = null;
+    }
+
+    public push(line: string): ILine {
+        const log: ILine = {
+            text: line,
+            tag: createTag(this.level)
+        };
+        this.message.push(log);
+        return log;
+    }
+
+    public pushTrace(line: string): ILine {
+        const log: ILine = {
+            text: line,
+            tag: createTag(this.level)
+        };
+        this.trace.push(log);
+        return log;
+    }
+
+    public pushCause(type: string): LogException {
+        const exc: LogException = LogException.create(type, this.level);
+        this.cause = exc;
+        return exc;
+    }
+
+    public build(tag: HTMLDivElement): void {
+        for (let line of this.message) {
+            tag.appendChild(line.tag);
+        }
+        for (let line of this.trace) {
+            tag.appendChild(line.tag);
+        }
+        if (this.cause !== null) {
+            this.cause.build(tag);
+        }
+    }
+
+    public update(options: IOptions, is_cause: boolean = false): void {
+        
+        const hide: boolean = !options.levelFor(this.level, true);
+
+        let first: boolean = true;
+        for (let line of this.message) {
+            let text: string = line.text;
+            if (first) {
+                first = false;
+                text = `${is_cause ? 'Caused by: ' : ''}${this.type}: ${text}`;
+            }
+            line.tag.hidden = hide;
+            line.tag.innerHTML = encodeHtml(text);
+        }
+
+        for (let line of this.trace) {
+            line.tag.hidden = hide || !options.stacktrace;
+            line.tag.innerHTML = encodeHtml(line.text);
+        }
+
+        if (this.cause !== null) {
+            this.cause.update(options, true);
+        }
+    }
+}
+
+
+export class LogLine {
+
+    public static create(time: string, source: string, level: LevelInfo): LogLine {
+        return new LogLine(time, source, level);
+    }
+
+
+    public readonly message: ILine[];
+    public readonly exceptions: LogException[];
+
+    private readonly time: string;
+    private readonly source: string;
+    private readonly level: LevelInfo;
+
+    private constructor(time: string, source: string, level: LevelInfo) {
+        this.message = [];
+        this.exceptions = [];
+
+        this.time = time;
+        this.source = source;
+        this.level = level;
+    }
+
+    public push(line: string): ILine {
+        const log: ILine = {
+            text: line,
+            tag: createTag(this.level)
+        };
+        this.message.push(log);
+        return log;
+    }
+
+    public pushException(type: string): LogException {
+        const exc: LogException = LogException.create(type, this.level);
+        this.exceptions.push(exc);
+        return exc;
+    }
+
+    public build(tag: HTMLDivElement): void {
+        for (let line of this.message) {
+            tag.appendChild(line.tag);
+        }
+        for (let exc of this.exceptions) {
+            exc.build(tag);
+        }
+    }
+
+    public update(options: IOptions): void {
+        const hide: boolean = !options.levelFor(this.level, true);
+
+        let first = true;
+        for (let line of this.message) {
+            let text = line.text;
+            if (first) {
+                first = false;
+                text = this.updatePrefix(text, options);
+            }
+
+            line.tag.hidden = hide;
+            line.tag.innerHTML = encodeHtml(text);
+        }
+
+        for (let exc of this.exceptions) {
+            exc.update(options);
+        }
+    }
+
+    private updatePrefix(text: string, options: IOptions): string {
+        if (options.source && options.level) {
+            text = `[${this.source} / ${this.level}] ${text}`;
+        } else if (options.source) {
+            text = `[${this.source}] ${text}`;
+        } else if (options.level) {
+            text = `[${this.level}] ${text}`;
+        }
+
+        if (options.timestamps) {
+            text = `[${this.time}] ${text}`;
+        }
+
+        return text;
+    }
+}
+
+function createTag(level: LevelInfo | null): HTMLParagraphElement {
+    const tag: HTMLParagraphElement = document.createElement('p');
+    if (level !== null) {
+        tag.classList.add(`log-${level.toLowerCase()}`);
+    }
+    return tag;
+}
+
+const HTML_ESCAPE_ENTITIES: {[key: string]: string} = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;',
+    '`': '&#x60;',
+    '=': '&#x3D;'
+};
+
+
+function encodeHtml(text: string): string {
+    // if (text.includes('§')) {
+    //    text = text.replace(/[\&<>"'`=\/]/g, s => HTML_ESCAPE_ENTITIES[s] as string);
+    //}
+    //text = text.replace(/[\&<>"'`=\/]/g, s => HTML_ESCAPE_ENTITIES[s] as string);
+    return text.replaceAll('\t', '    ').replaceAll(' ', '&nbsp;');
+}
